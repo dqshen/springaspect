5fbb47cab8db96aacea8395a102277c1c6153b9b
dqshen <dqshen@hogwarts>
2022-03-26 21:59:40 +0800

    增加关于切面和引入配置的关键注释

--- src/main/java/com/example/springaspect/ConcertConfig.java
+++ src/main/java/com/example/springaspect/ConcertConfig.java
@@ -10,11 +10,13 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan
 public class ConcertConfig {
     
+    // 注意切面需要在配置类或者xml配置文件额外声明成bean，而不像一般Acappella这样的Component那样通过ComponentScan就可以进入上下文
     @Bean
     public Audience audience(){
         return new Audience();
     }
 
+    // 和切面一样，introducation引入语法需要在配置类或者xml配置文件额外声明成bean，仅靠@ComponentScan是不能让引入生效的
     @Bean
     public EncoreableIntroducer introducer(){
         return new EncoreableIntroducer();
